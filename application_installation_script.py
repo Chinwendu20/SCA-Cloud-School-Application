68b1741419d252a0821290c2c6203ff6e558e831
Chinwendu20 <amaka013@gmail.com>
2021-01-15 00:03:32 +0100

    This scripts checks if application, wget, curl and nodejs are installed on a system. If not installed the script installs it using their appropriate command

---
+++ application_installation_script.py
@@ -0,0 +1,81 @@
+import os 
+import platform
+import subprocess
+
+print('Hi there')
+#Dictionary mapping each operating system with its installation command
+Dictionary_of_Os_with_installation_command={ 'Darwin':[ 'port','install'], 'Linux':[ 'sudo','apt-get', 'install'],'Windows': ['choco','install']}
+
+#Dictionary mapping each application with their package name used for installation
+Dictionary_of_applications_to_install={ 'node':'nodejs', 'curl':'curl', 'wget':'wget'}
+
+
+def checking_operating_system():
+	"""Checking operating system script is running on"""
+	
+	print('Checking operating system...')
+	
+	#Using the plafform.system() to determine operating system of machine runing the script
+	operating_system=platform.system()
+	print("Gotten, {}'s environment. Let's go..".format(operating_system))
+
+	return operating_system
+
+
+def Fetching_installation_command_for_OS():
+	"""Fetches installation command for system's operating sytem"""
+	
+	#Calling checking_operating_system to obatain system's operating system
+	operating_system=checking_operating_system()
+	print("Fetching installation command for {}'s system".format(operating_system))
+
+	#Obtaining specific installation command for system's operating system
+	installation_command_for_OS=Dictionary_of_Os_with_installation_command[operating_system]
+	
+	return installation_command_for_OS
+
+
+def installing_apps(application):
+	"""Installs application after checks"""
+	
+	#Calling the Fetching_installation_command_for_os to obtain installation command for system's operating system
+	installation_command_for_OS=Fetching_installation_command_for_OS()
+	
+	#Appending the application to be installed in to list of installation commands
+	installation_command_for_OS.append(Dictionary_of_applications_to_install[application])
+
+	
+	print('Installing {} .....'.format(application))
+	
+	#Calling the installation command for the application
+	subprocess.run(installation_command_for_OS)		
+	
+
+def checking_if_apps_are_already_installed():
+	"""Checks if application is already installed, if not installed, installs it"""
+	
+	for application in Dictionary_of_applications_to_install:
+		try:
+			
+			#Running command to check if application is on system
+			subprocess.run([application, '--help'], capture_output=True)
+			print('It is')
+		except:
+			print('{}, not on system'.format(application))
+
+			#Calling installing_apps() function to install application
+			installing_apps(application)
+
+
+print('checking if system has required applications')
+
+#Calling function to start the whole process
+checking_if_apps_are_already_installed()
+print('All done')
+
+
+
+
+	
+
+
